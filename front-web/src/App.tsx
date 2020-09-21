3f3aaed2cf2a26f1ef5f9922943a21f97946804f
Guilherme Cleveland <axguilherme@gmail.com>
2020-09-20 23:24:10 -0300

    Fix

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
 import Routes from './Routes';
 
 
